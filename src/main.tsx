38ddcb9625ec52ba1d8f06b8bd2f16593c6403ce
Luca Allievi <luca@gr4vy.com>
2025-11-13 16:36:36 +0100

    task: replace poutine with canary version and add prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@gr4vy/poutine-react/dist/poutine-react.css'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
