562e47db5bf930298b0e32f74f74ab68b64d053b
Luca Allievi <luca@gr4vy.com>
2025-12-13 18:29:33 +0100

    fix: load overlay flow for overlay route

--- src/routes/overlay.tsx
+++ src/routes/overlay.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Checkout } from '@/components'
-import { Inline as Component } from '@/flows'
+import { Overlay as Component } from '@/flows'
 
 export const Route = createFileRoute('/overlay')({
   component: Overlay,
